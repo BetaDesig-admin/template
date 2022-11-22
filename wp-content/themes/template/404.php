2c5c5ad81b0e86140024a6bcc01edce9a92c805a
BetaDesign-Gracia <charlotte@betadesign.dk>
2022-11-22 15:04:29 +0100

    404 lavet mere standard

--- wp-content/themes/template/404.php
+++ wp-content/themes/template/404.php
@@ -1,14 +1,12 @@
 <?php get_header(); ?>
     <section class="page_not_found">
         <div class="container">
-            <h1 class="squiggly primary-dark">Siden blev <br> ikke fundet</h1>
+            <h1>Siden blev ikke fundet</h1>
             <p>
-                <strong>Er du farret vild?</strong>
+                <strong>Er du faret vild?</strong>
             </p>
-            <p>
-                Siden du leder efter blev ikke fundet, du kan komme tilbage på forsiden
-            </p>
-            <a class="straight primary-dark" href="/">Klik her for flere farve eventyr</a>
+            <p>Siden du leder efter blev ikke fundet</p>
+            <a href="/">Klik her for at komme tilbage til forsiden</a>
         </div>
     </section>
 <?php get_footer(); ?>
