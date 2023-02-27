6152f78dd07f188c6cbab15ae87bdcd4531688e4
BetaDesign-Gracia <charlotte@betadesign.dk>
2023-02-27 09:20:49 +0100

    copyright i footer

--- wp-content/themes/template/footer.php
+++ wp-content/themes/template/footer.php
@@ -1,6 +1,8 @@
 <footer>
     <div class="container">
-
+        <section class="copyright">
+            <p>© <?php echo date("Y"); ?> All rights reserved</p>
+        </section>
     </div>
 </footer>
 <?php wp_footer(); ?>
